745952ba358b47114e8f56958d15293faa35adfe
Inna949Festchuk <innabori@mail.ru>
2025-04-13 02:43:09 +0200

    Feature not available in your country.

--- main.py
+++ main.py
@@ -12,64 +12,82 @@ from folium.plugins import HeatMap
 from shapely.geometry import Polygon  # Работа с геометрией
 
 # Настройки OSMnx для кеширования запросов
-ox.settings.use_cache = True
-ox.settings.log_console = True
+# ox.settings.use_cache = True
+# ox.settings.log_console = True
 
 def get_city_boundary(city_name):
     """
     Получает административные границы города из OSM.
-
     Параметры:
         city_name (str): Название города на русском языке
-
     Возвращает:
         GeoDataFrame: Геоданные с границами города
     """
     try:
+        # Загружаем данные административных границ
+        gdf = ox.features_from_place(
+            f'{city_name}, Россия',
+            tags={'boundary': 'administrative'}
+        )
+
+        # Фильтруем городской округ
+        filtered = gdf[
+            (gdf['name'].str.contains(city_name, na=False)) &
+            (gdf['admin_level'] == '6')  # Уровень для городов федерального значения
+        ]
+
+        if not filtered.empty:
+            return filtered.reset_index()
+
         # Геокодирование
-        return ox.geocode_to_gdf(f'{city_name}, Россия', which_result=1) 
+        # return ox.geocode_to_gdf(f'{city_name}, Россия', which_result=1) 
+        return gpd.GeoDataFrame()
 
     except Exception as e:
         print(f'Ошибка загрузки данных: {e}')
         return gpd.GeoDataFrame()  # Возвращаем пустой DataFrame
 
 
-def visualize_city_boundary(gdf):
+def visualize_city_boundary(city_gpf):
     """
     Визуализирует границы города на карте.
-
     Параметры:
         gdf (GeoDataFrame): Геоданные с полигонами
-
     Возвращает:
         folium.Map: Карта с отрисованными границами
     """
-    if gdf.empty:
+    if city_gpf.empty:
         print("Нет данных для визуализации")
         return folium.Map(location=[55.751244, 37.618423], zoom_start=10)  # Москва по умолчанию
 
     # Создаем базовую карту
-    centroid = gdf.union_all().centroid  # Находим центр города
+    centroid = city_gpf.union_all().centroid  # Находим центр города
 
     map_city = folium.Map(
         location=[centroid.y, centroid.x],
         zoom_start=12,
-        tiles='cartodbpositron'
+        tiles='CartoDB positron', # Плитки карты остаются такими же
+        name='Базовая карта', # Имя для панели слоев
+        control=True
     )
+    
+    # Добавляем слой границ города
+    city_layer = folium.FeatureGroup(name='Границы города', show=True)
+    city_layer.add_to(map_city)
 
-    # Добавляем GeoJSON слой
+    # Добавляем GeoJSON в слой city_layer
     folium.GeoJson(
-        gdf.__geo_interface__, # — это специальный интерфейс в Python, \
+        city_gpf.__geo_interface__, # — это специальный интерфейс в Python, \
         # который используется для стандартизации работы с географическими \
         # данными различных геобиблиотек
-        name='Границы города',
+        name='Границы города',  # Здесь задаете имя слоя для границ города
         style_function=lambda x: {
             'fillColor': '#a1a1a1',  # Цвет заливки
             'color': 'red',  # Цвет границ
-            'weight': 2  # Толщина границ
+            'weight': 5  # Толщина границ
         },
         tooltip=folium.GeoJsonTooltip(fields=['name'], aliases=['Город:']) # Всплывающее окно
-    ).add_to(map_city)
+    ).add_to(city_layer)
 
     return map_city
 
@@ -77,10 +95,8 @@ def visualize_city_boundary(gdf):
 def create_hexagons(geoJson, base_map):
     """
     Генерация H3-гексагона внутри заданного полигона и визуализация их на карте.
-
     Параметры:
         geoJson (dict): GeoJSON с полигоном
-
     Возвращает:
         folium.Map: Карта с визуализированными гексагонами
         hexagons (list): Список H3 идентификаторов
@@ -100,13 +116,14 @@ def create_hexagons(geoJson, base_map):
     lat_lng_poly = h3.LatLngPoly(coordinates)
 
     # Генерация H3-гексагона
-    hexagons = h3.h3shape_to_cells(lat_lng_poly, res=50)  # Уровень детализации 10
+    hexagons = h3.h3shape_to_cells(lat_lng_poly, res=8)  # Уровень детализации 10
 
-    # Создаем карту
-    # m = folium.Map(location=[np.mean([coord[1] for coord in coordinates]), np.mean([coord[0] for coord in coordinates])],
-    #                   zoom_start=13, tiles='cartodbpositron')
+    # Визуализируем гексагоны
 
-    # Визуализируем каждый гексагон
+    # Создаем слой гексагонов
+    layer_hexagon = folium.FeatureGroup(name=f'Гексагоны', show=True)
+    layer_hexagon.add_to(base_map)
+
     for hex_id in hexagons:
         # Получаем LatLngPoly для данного H3 идентификатора
         polygon = h3.cells_to_h3shape([hex_id], tight=True)
@@ -118,9 +135,10 @@ def create_hexagons(geoJson, base_map):
         polyline = list(outlines) + [outlines[0]]  # Замыкаем полигон
 
         # Добавляем на карту
-        folium.PolyLine(locations=polyline, weight=3, color='red').add_to(base_map)
+        map_hexagon = folium.PolyLine(locations=polyline, weight=3, color='grey').add_to(layer_hexagon)
 
-    return hexagons, base_map
+    
+    return hexagons, map_hexagon
 
 if __name__ == "__main__":
     try:
@@ -144,9 +162,12 @@ if __name__ == "__main__":
         }
 
         # Вызов функции для создания гексагонов
-        hexagons, hex_map = create_hexagons(geoJson, city_map)
+        create_hexagons(geoJson, city_map)
         
-        hexagons.save("output_map.html")
+        # Кнопка управления слоями
+        folium.LayerControl().add_to(city_map)
+        
+        city_map.save("output_map.html")
 
         print("Карта успешно сохранена")
     except Exception as e:
